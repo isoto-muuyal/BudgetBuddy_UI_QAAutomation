dad05055818cfb26d46e054b5c4b5a0e2334e10f
isoto soto <israelss@gmail.com>
2026-03-10 18:18:57 -0400

    init

---
+++ src/main/java/com/muuyal/qa/budgetwise_automation/BudgetwiseAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.muuyal.qa.budgetwise_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetwiseAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetwiseAutomationApplication.class, args);
+	}
+
+}
